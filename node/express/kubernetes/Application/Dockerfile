506152371e57d6639f97f058d996282e15a5c729
Ranganat Govardhanagiri <rgovardh@microsoft.com>
2018-04-30 14:13:59 +0530

    AKS Sample for Node Express

---
+++ node/express/kubernetes/Application/Dockerfile
@@ -0,0 +1,16 @@
+FROM node:8-onbuild
+MAINTAINER Azure App Services Container Images <appsvc-images@microsoft.com>
+ENV PORT 8080
+EXPOSE 8080
+
+# Create app directory
+WORKDIR /app
+COPY package.json .
+
+# Install app dependencies
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+CMD ["npm", "start"]
---
+++ node/express/kubernetes/Application/routes/index.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var redis = require('ioredis');
+
+console.log("trying to create redis client");
+var redisServer = process.env.redis_server || 'redis-cache';
+console.log(redisServer);
+var client = redis.createClient(6379, redisServer);
+console.log("redis client created");
+client.set('foo', 'bar');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    console.log("Index route");
+    client.get('foo', function (err, result) {
+      console.log('ioredis get completed');  
+      console.log(result);
+      res.render('index', { title: result });
+    });     
+});
+
+module.exports = router;

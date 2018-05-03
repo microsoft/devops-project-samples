22e0e2f22b756bbd81da457ea8144b2a62df6f5d
Deepak Sattiraju <desattir@microsoft.com>
2018-05-03 19:13:42 +0530

    Removing redis from plain js

--- node/plain/kubernetes/Application/server.js
+++ node/plain/kubernetes/Application/server.js
@@ -6,24 +6,7 @@ const appInsights = require('applicationinsights');
 appInsights.setup();
 appInsights.start();
 
-var redis = require('ioredis');
-
-console.log("trying to create redis client");
-var redisServer = process.env.redis_server || 'redis-cache';
-console.log(redisServer);
-var client = redis.createClient(6379, redisServer);
-console.log("redis client created");
-client.set('foo', 'barplain');
-
-console.log("Index route");
-client.get('foo', function (err, result) {
-    console.log('ioredis get completed');  
-    console.log(result);
-});     
-
-
 http.createServer(function (req, res) {
-
     fs.readFile('index.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
         res.write(data);

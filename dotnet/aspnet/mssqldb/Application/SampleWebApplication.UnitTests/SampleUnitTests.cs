9e19f69b8e3ea6b59b0d6593c2617c68689b727d
Shreyas R <shrer@microsoft.com>
2018-08-20 16:00:27 +0530

    Added Test Projects phase 1

---
+++ dotnet/aspnet/mssqldb/Application/SampleWebApplication.UnitTests/SampleUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleWebApplication.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SampleUnitTest1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void SampleUnitTest2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void SampleUnitTest3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

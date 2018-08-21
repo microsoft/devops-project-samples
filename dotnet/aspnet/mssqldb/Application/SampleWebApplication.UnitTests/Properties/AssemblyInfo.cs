0bee5e288a75b1fe1d14452ea7661a815a42d77c
Shreyas R <shrer@microsoft.com>
2018-08-21 11:10:57 +0530

    Cherry pick commit for 139 (#48)
    * Added Test Projects phase 1
    
    * Fixed framework version

---
+++ dotnet/aspnet/mssqldb/Application/SampleWebApplication.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleWebApplication.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleWebApplication.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f34965fd-e751-4f15-a5d0-759436e0c1b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

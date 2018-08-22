e8de2a4836418533e679f15b334bc4ddb92cbdb6
Shreyas R <shrer@microsoft.com>
2018-08-22 11:31:13 +0530

    Replicated dot net folder

--- dotnetWithTest/aspnet46/vm-mssqldb/Application/Database1/Table1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE accessLogs
-(
-ID_column INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-   PageName  nvarchar(128)     not null, 
-    [AccessDate]  DATETIME NOT NULL DEFAULT GETUTCDATE() 
-);
--- dotnetWithTest/aspnetcore/vm-mssqldb/Application/Database1/Table1.sql
+++
@@ -1,8 +0,0 @@
-﻿if not exists (select * from sysobjects where name='accessLogs' and xtype='U')
-CREATE TABLE accessLogs
-(
-ID_column INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-   PageName  nvarchar(128)     not null, 
-    [AccessDate]  DATETIME NOT NULL DEFAULT GETUTCDATE() 
-)
-go

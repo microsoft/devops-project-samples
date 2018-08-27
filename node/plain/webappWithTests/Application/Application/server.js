2e6aecd27f0b0c8f19e1f79e85e846914948084a
Shreyas R <shrer@microsoft.com>
2018-08-27 21:15:46 +0530

    Added UTs and FTs

--- node/plain/webappWithTests/Application/Application/server.js
+++ node/plain/webappWithTests/Application/Application/server.js
@@ -3,11 +3,20 @@ var http = require('http');
 var port = process.env.PORT || 8092;
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-
+var server = http.createServer(function (req, res) {
     fs.readFile('index.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
         res.write(data);
         res.end();
     });
-}).listen(port);
+});
+
+exports.listen = function () {
+    server.listen.apply(server, arguments);
+};
+  
+exports.close = function (callback) {
+    server.close(callback);
+};
+
+server.listen(port);

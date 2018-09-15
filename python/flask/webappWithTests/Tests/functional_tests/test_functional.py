5e0553a582e6791d52ec90d377497eae21b61877
Vagisha Nidhi <vagisha.nda@gmail.com>
2018-09-15 21:59:40 +0530

    Adding tests to flask sample

---
+++ python/flask/webappWithTests/Tests/functional_tests/test_functional.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+import unittest
+import os
+import sys
+import pytest
+
+class FunctionalTests(unittest.TestCase):
+
+	def setUp(self):
+		options = webdriver.ChromeOptions()
+		options.add_argument('--no-sandbox')
+		self.driver = webdriver.Chrome(os.environ["ChromeWebDriver"], chrome_options=options)
+
+	def test_selenium(self):
+		try:
+			webAppUrl = pytest.config.getoption('webAppUrl')
+			response = self.driver.get(webAppUrl)
+			html_source = self.driver.page_source
+			self.assertIn("<title>Home Page - Python Flask Application</title>", html_source)
+		except AssertionError:
+			raise
+		except Exception as e:
+			sys.stderr.write('tests_selenium.Error occurred while executing tests: ' + str(e))
+
+	def tearDown(self):
+		try:
+			self.driver.quit()
+		except Exception as e:
+			print('tearDown.Error occurred while trying to close the selenium chrome driver: ' + str(e))

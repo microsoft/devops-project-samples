3e6137139939d82968f5119b045583f778fa9e9f
Vagisha Nidhi <vagisha.nda@gmail.com>
2018-09-18 11:16:32 +0530

    Comment added

--- python/bottle/webappWithTests/Tests/functional_tests/test_functional.py
+++ python/bottle/webappWithTests/Tests/functional_tests/test_functional.py
@@ -12,6 +12,12 @@ class FunctionalTests(unittest.TestCase):
 		options.add_argument('--no-sandbox')
 		self.driver = webdriver.Chrome(os.environ["ChromeWebDriver"], chrome_options=options)
 
+	"""
+	The current time taken by the webapp to refresh after deployment is a considerable amount and the selenium tests
+	in the release run much faster than this duration, hence the tests do not assert the current deployed app but from
+	the last deployment (in case of the first deployment it happens to be the default iis page). Hence the try catch 
+	around the title assertion which is a temporary solution until the webapp deployment refresh times are fixed.
+	"""
 	def test_selenium(self):
 		try:
 			webAppUrl = pytest.config.getoption('webAppUrl')

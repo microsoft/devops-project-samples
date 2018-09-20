bdeb2c618324f7a6a07fdc989d7319f1fc3df9b0
Vagisha Nidhi <vanidhi@microsoft.com>
2018-09-20 11:46:18 +0530

    Adde chromedriver.exe to chromedriver path

--- python/django/containerWithTests/Tests/functional_tests/test_functional.py
+++ python/django/containerWithTests/Tests/functional_tests/test_functional.py
@@ -1,6 +1,3 @@
-"""
-This file demonstrates writing functional tests.
-"""
 import pytest
 from selenium import webdriver
 import unittest
@@ -10,33 +7,34 @@ import pytest
 
 class FunctionalTests(unittest.TestCase):
 
-    def setUp(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument('--no-sandbox')
-        self.driver = webdriver.Chrome(os.environ["ChromeWebDriver"], chrome_options=options)
+	def setUp(self):
+		options = webdriver.ChromeOptions()
+		options.add_argument('--no-sandbox')
+		self.driver = webdriver.Chrome(os.path.join(os.environ["ChromeWebDriver"], 'chromedriver.exe'), chrome_options=options)
 
-    """
-    Sample selenium test. Asserting webapp page title and since webapp deployment takes
-    time until the tests are run, we assert default page title until the issue is not 
-    with the webapp deployment. We shall remove the default title assertion once the issue is fixed.
-    """
-    def test_selenium(self):
-        try:
-            webAppUrl = pytest.config.getoption('webAppUrl')
-            response = self.driver.get(webAppUrl)
-            html_source = self.driver.page_source
-            self.assertIn("<title>Home Page - Python Django Application</title>", html_source)
-        except AssertionError:
-            try:
-                # Default title assertion. Remove when deployment issue is fixed
-                self.assertIn("<title>Microsoft Azure App Service - Welcome</title>", html_source)
-            except AssertionError:
-                raise
-        except Exception as e:
-            sys.stderr.write('tests_selenium.Error occurred while executing tests: ' + str(e))
+	"""
+	The current time taken by the webapp to refresh after deployment is a considerable amount and the selenium tests
+	in the release run much faster than this duration, hence the tests do not assert the current deployed app but from
+	the last deployment (in case of the first deployment it happens to be the default iis page). Hence the try catch 
+	around the title assertion which is a temporary solution until the webapp deployment refresh times are fixed.
+	"""
+	def test_selenium(self):
+		try:
+			webAppUrl = pytest.config.getoption('webAppUrl')
+			response = self.driver.get(webAppUrl)
+			html_source = self.driver.page_source
+			self.assertIn("<title>Home Page - Python Django Application</title>", html_source)
+		except AssertionError:
+			try:
+				# Default title assertion. Remove when deployment issue is fixed
+				self.assertIn("<title>Microsoft Azure App Service - Welcome</title>", html_source)
+			except AssertionError:
+				raise
+		except Exception as e:
+			sys.stderr.write('tests_selenium.Error occurred while executing tests: ' + str(e))
 
-    def tearDown(self):
-        try:
-            self.driver.quit()
-        except Exception as e:
-            print('tearDown.Error occurred while trying to close the selenium chrome driver: ' + str(e))
+	def tearDown(self):
+		try:
+			self.driver.quit()
+		except Exception as e:
+			print('tearDown.Error occurred while trying to close the selenium chrome driver: ' + str(e))

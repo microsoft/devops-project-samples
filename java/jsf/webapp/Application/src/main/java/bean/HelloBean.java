3e744df5c77cf396e6ac5b5e0f898d8603cad186
Alex Aquino <alexaquino.it@gmail.com>
2018-10-03 22:01:42 -0300

    Migrate JSF managed beans (deprecated) to CDI managed beans

--- java/jsf/webapp/Application/src/main/java/bean/HelloBean.java
+++ java/jsf/webapp/Application/src/main/java/bean/HelloBean.java
@@ -2,17 +2,18 @@ package bean;
 
 import java.io.Serializable;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
-@ManagedBean
+@Named
 @SessionScoped
-public class HelloBean  implements Serializable{
+public class HelloBean implements Serializable {
 
 	private static final long serialVersionUID = -4823295172962937652L;
 	
 	private String message = "Your Java app is up and running on Azure";
-
+	
+	
 	public String getMessage() {
 		return message;
 	}

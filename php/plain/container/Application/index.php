c4d2b27525eb189d020c51b08c992e5517555eda
Gabriel Machado <gabriel.ms1@hotmail.com>
2018-11-05 09:44:53 -0200

    Remove extra spaces on PHP plain sample (#105)
    * Insert space before var asign
    
    * Remove extra spaces

--- php/plain/container/Application/index.php
+++ php/plain/container/Application/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'vendor/autoload.php';
-    $app_insights_instrumentation =getenv('APPINSIGHTS_INSTRUMENTATIONKEY');
+    $app_insights_instrumentation = getenv('APPINSIGHTS_INSTRUMENTATIONKEY');
     $telemetryClient = new \ApplicationInsights\Telemetry_Client();
     $telemetryClient->getContext()->setInstrumentationKey($app_insights_instrumentation);
     $telemetryClient->trackRequest('Server Requests','Azure DevOps Project', time());
@@ -25,7 +25,7 @@
             <div class="tweet-container">
             <a href="http://twitter.com/intent/tweet/?text=I%20just%20created%20a%20new%20PHP%20website%20on%20Azure%20using%20Azure%20DevOps%20Project&hashtags=AzureDevOpsProject%2CVSTS%20%40Azure%20%40VSTS">
                 <img src="img/tweetThis.svg" alt="tweetThis" />
-            </a>            
+            </a>
         </div>
             <div class="content-body">
                 <div class="success-text">Success!</div>
--- php/plain/webapp/Application/index.php
+++ php/plain/webapp/Application/index.php
@@ -16,7 +16,7 @@
             <div class="tweet-container">
             <a href="http://twitter.com/intent/tweet/?text=I%20just%20created%20a%20new%20PHP%20website%20on%20Azure%20using%20Azure%20DevOps%20Project&hashtags=AzureDevOpsProject%2CVSTS%20%40Azure%20%40VSTS">
                 <img src="img/tweetThis.svg" alt="tweetThis" />
-            </a>            
+            </a>
         </div>
             <div class="content-body">
                 <div class="success-text">Success!</div>

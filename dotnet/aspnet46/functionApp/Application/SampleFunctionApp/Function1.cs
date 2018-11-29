8c2d795e50e37a8b878382d64efe66f0dddc2eb0
Nitin Issac Joy <nitin.i.joy@gmail.com>
2018-11-29 10:42:57 +0530

    Sample Function App for .NET 4.6 (#114)
    * Sample Function App for .NET 4.6
    
    * Moving sample code to Application folder
    
    * Renaming to functionApp
    
    * Fixing ARM Template
    
    * Adding Function Version to ARM Template

---
+++ dotnet/aspnet46/functionApp/Application/SampleFunctionApp/Function1.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace SampleFunctionApp
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            if (name == null)
+            {
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                name = data?.name;
+            }
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.OK, "Hello there! Welcome to Azure Functions!")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name + "! Welcome to Azure Functions!");
+        }
+    }
+}

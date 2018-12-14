ad30967f6a4b006ff4262befac59b264d98e8d4a
Nitin Issac Joy <nijoy@microsoft.com>
2018-12-14 17:40:02 +0530

    Adding Test project for Azure Functions .NET Core

---
+++ dotnet/aspnetcore/functionApp/Application/SampleFunctionApp.Test/ListLogger.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleFunctionApp.Test
+{
+    public class ListLogger : ILogger
+    {
+        public IList<string> Logs;
+
+        public IDisposable BeginScope<TState>(TState state) => NullScope.Instance;
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public ListLogger()
+        {
+            this.Logs = new List<string>();
+        }
+
+        public void Log<TState>(LogLevel logLevel,
+                                EventId eventId,
+                                TState state,
+                                Exception exception,
+                                Func<TState, Exception, string> formatter)
+        {
+            string message = formatter(state, exception);
+            this.Logs.Add(message);
+        }
+    }
+}

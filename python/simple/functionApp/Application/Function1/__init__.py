99292e3184ce41e0647911226809c25b447c4bbd
Nitin Issac Joy <nijoy@microsoft.com>
2019-02-07 17:21:38 +0530

    Python function sample code

---
+++ python/simple/functionApp/Application/Function1/__init__.py
@@ -0,0 +1,19 @@
+import logging
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello {name}! Welcome to Azure Functions!")
+    else:
+        return func.HttpResponse(f"Hello! Welcome to Azure Functions!")

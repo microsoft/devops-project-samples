d23b216b607d59247a7336150fbb59d0582c5010
Vineet <vineet.mimrot@gmail.com>
2019-08-21 18:56:04 +0530

    update app message (#195)

--- java/jsf/webapp/Application/src/main/java/bean/HelloBean.java
+++ java/jsf/webapp/Application/src/main/java/bean/HelloBean.java
@@ -11,7 +11,7 @@ public class HelloBean  implements Serializable{
 
 	private static final long serialVersionUID = -4823295172962937652L;
 	
-	private String message = "Your Java app is up and running on Azure";
+	private String message = "Your Java JSF app is up and running on Azure";
 
 	public String getMessage() {
 		return message;

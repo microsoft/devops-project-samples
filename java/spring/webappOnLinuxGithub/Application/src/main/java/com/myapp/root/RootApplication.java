b8079c1f71d2a143bb7133110aa43e1605fac630
Anand Upadhyay <anupadh@microsoft.com>
2020-06-23 03:55:53 +0530

    Sample starter project for java with gradle build for java with latest spring boot

---
+++ java/spring/webappOnLinuxGithub/Application/src/main/java/com/myapp/root/RootApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootApplication.class, args);
+	}
+
+}

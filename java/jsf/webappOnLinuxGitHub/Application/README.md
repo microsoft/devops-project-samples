9899b60762d2ea86464fe66c40b5fe63233d090c
Anand Upadhyay <upadhyayap.smit@gmail.com>
2020-06-25 14:28:17 +0530

    Starter code for java jsf based web apps for devops starter on github (#235)
    * starter code for java jsf stack for github starter
    
    * Added Starter code for java jsf web app for both linux and windows platform with functional tests
    
    Co-authored-by: Anand Upadhyay <anupadh@microsoft.com>

---
+++ java/jsf/webappOnLinuxGitHub/Application/README.md
@@ -0,0 +1,18 @@
+| Language | Framework | Platform | Author |
+| -------- | -------- |--------|--------|
+| Java | JSF | Azure Web App, Virtual Machine| |
+
+
+# JAVA JSF web application
+
+Sample Java JSF web application that you can deploy to Azure.
+
+
+## License:
+
+See [LICENSE](LICENSE).
+
+## Contributing
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
---
+++ java/jsf/webappOnWindowsGitHub/Application/sampleWebApp/src/test/java/unit_tests/SampleUnitTest.java
@@ -0,0 +1,30 @@
+package unit_tests;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.*;
+import org.richfaces.test.AbstractFacesTest;
+import org.richfaces.test.LocalWebClient;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class SampleUnitTest extends AbstractFacesTest {
+
+		private String viewsPath = "src" + File.separator + "main" + File.separator + "webapp";
+
+		@Override
+    protected void setupWebContent() {
+        String webappDirectory = Paths.get(System.getProperty("user.dir"), viewsPath).toString();      
+        File webFile = new File(webappDirectory);
+        facesServer.addResourcesFromDirectory("/", webFile);
+    }
+
+    @Test
+    public void testTitleAssert() throws Exception {
+        WebClient webClient = new LocalWebClient(facesServer);
+        HtmlPage page = webClient.getPage("http://localhost/index.jsf");
+        assertEquals("Sample JSF Application", page.getTitleText());
+    }
+}

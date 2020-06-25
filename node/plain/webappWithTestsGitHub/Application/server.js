1babb07ccc57c2401a18055ed425efd05d861df1
Anand Upadhyay <upadhyayap.smit@gmail.com>
2020-06-25 14:29:09 +0530

    Starter code simple node js flow (#233)
    * Starter code simple node js flow
    
    * updated tweet links
    
    * updated tweet link and text
    
    * updated landing page title
    
    Co-authored-by: Anand Upadhyay <anupadh@microsoft.com>

---
+++ node/plain/webappWithTestsGitHub/Application/server.js
@@ -0,0 +1,22 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 8092;
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        res.write(data);
+        res.end();
+    });
+});
+
+exports.listen = function () {
+    server.listen.apply(server, arguments);
+};
+  
+exports.close = function (callback) {
+    server.close(callback);
+};
+
+server.listen(port);

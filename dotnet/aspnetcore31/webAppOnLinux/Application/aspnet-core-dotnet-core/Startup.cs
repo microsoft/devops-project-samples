4fb19ffd158bb6fbda8254911c7f64368f030470
Ninad Kavimandan <ninadkavimandan@github.com>
2020-07-16 13:30:33 +0530

    update to 3.1 till webAppOnLinux

--- dotnet/aspnetcore31/webAppOnLinux/Application/aspnet-core-dotnet-core/Startup.cs
+++ dotnet/aspnetcore31/webAppOnLinux/Application/aspnet-core-dotnet-core/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace aspnet_core_dotnet_core
 {
@@ -31,11 +32,11 @@ namespace aspnet_core_dotnet_core
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -48,8 +49,12 @@ namespace aspnet_core_dotnet_core
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseRouting();
+            app.UseAuthorization();
 
-            app.UseMvc();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapRazorPages();
+            });
         }
     }
 }

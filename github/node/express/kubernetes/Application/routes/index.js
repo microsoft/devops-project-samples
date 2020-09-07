5b5ac334ac8862302fad5768c74c23b196fa66c9
Suyash Saluja <susaluj@microsoft.com>
2020-09-07 13:55:39 +0530

    Adding kubernetes flow for GitHub

---
+++ github/node/express/kubernetes/Application/routes/index.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var redis = require('ioredis');
+
+var redisServer = process.env.redis_server || 'redis-cache';
+
+console.log("Trying to create redis client");
+var client = redis.createClient(6379, redisServer);
+
+console.log("Redis Client created");
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  console.log("Index route");
+  client.exists('viewCount',function(err,reply) {
+        if(!err) {
+            if(reply === 1) {
+                client.incr('viewCount', (err, incrementedValue) => {
+                  if (err) {
+                  console.log(err);
+                  return;
+                }
+                res.render('index', { message: "Total Visits: " + incrementedValue }); 
+                });
+            } else {
+                client.set('viewCount', 1)
+            }
+        }
+    });
+});
+
+module.exports = router;

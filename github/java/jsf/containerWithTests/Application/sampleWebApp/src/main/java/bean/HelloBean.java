dc149dd662a762254b06ba8548f5db852fe6925f
chandra pratap prajapati <cpratap994@github.com>
2020-10-08 12:25:19 +0530

    initial commit

---
+++ github/java/jsf/containerWithTests/Application/sampleWebApp/src/main/java/bean/HelloBean.java
@@ -0,0 +1,23 @@
+package bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable{
+
+	private static final long serialVersionUID = -4823295172962937652L;
+	
+	private String message = "Your Java JSF app is up and running on Azure";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}

4f4441fae8700ac8ff3398ab0a5f7c8578db16e8
Chandra Pratap <cpratap994@github.com>
2020-11-11 23:39:43 +0530

    initial commit (#280)

---
+++ github/node/express/containerCosmosDBWithTests/Application/routes/index.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var dbOperations = require('../databaseOperations.js');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    dbOperations.addRecord("index", function(){
+        dbOperations.queryCount(function (count) {
+            res.render('index', {
+                title: 'Express',
+                count: count
+            });
+        }, function(error){
+            res.render('error', {
+                title: 'Express',
+                error: error
+            });
+        });
+    }, function(error){
+        res.render('error', {
+            title: 'Express',
+            error: error
+        });
+    });
+});
+
+module.exports = router;
